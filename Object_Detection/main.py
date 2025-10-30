213a4832a48627e4c22044f2e8cfa4cc63cbc136
rezjsh <149146755+rezjsh@users.noreply.github.com>
2025-10-30 15:44:03 +0330

    Add data validation stage to main pipeline

--- Object_Detection/main.py
+++ Object_Detection/main.py
@@ -1,6 +1,7 @@
 from src.utils.logging_setup import logger
 from src.config.configuration import ConfigurationManager
 from src.pipeline.stage_01_data_ingestion import DataIngestionPipeline
+from src.pipeline.stage_02_data_validation import DataValidationPipeline
 if __name__ == '__main__':
     try:
         config_manager = ConfigurationManager()
@@ -12,6 +13,12 @@ if __name__ == '__main__':
         data_ingestion_pipeline.run_pipeline()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 
+        # --- Data Validation Stage ---
+        STAGE_NAME = "Data Validation Stage"
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        data_validation_pipeline = DataValidationPipeline(config=config_manager)
+        data_validation_pipeline.run_pipeline()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
         logger.error(f"Error occurred during {STAGE_NAME} stage: {e}")
         raise e
